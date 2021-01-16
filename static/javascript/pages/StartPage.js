552c6f4f5b9bdfa27d2b7a6e5a141abf6e6e8064
choibumsu <bumsu0211@gmail.com>
2021-01-17 02:10:46 +0900

    start page

---
+++ static/javascript/pages/StartPage.js
@@ -0,0 +1,31 @@
+function StartPage() {
+	this.init = () => {
+		this.setElements()
+		this.bindEvents()
+	}
+
+	this.setElements = () => {
+		this.template = /*html*/ `
+      <div id="start-page">
+        <h1 class="title">🎊 Welcome To JavaScript Test ✍️</h1>
+        <button class="start-btn">START</button>
+      </div>
+    `
+
+		this.target = document.createElement('div')
+		this.target.innerHTML = this.template
+		this.target = this.target.firstElementChild
+
+		this.startBtn = this.target.querySelector('.start-btn')
+	}
+
+	this.bindEvents = () => {
+		this.startBtn.addEventListener('click', this.clickEventListener)
+	}
+
+	this.clickEventListener = () => {
+		router.route('quiz')
+	}
+
+	this.init()
+}
